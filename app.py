f18f7ed594f8c65d8a15aa974d7f7bc8afd8f907
Vedant-Soni <64590530+Vedant-Soni@users.noreply.github.com>
2020-09-28 00:11:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 18:57:23 2020
+
+@author: Vedant Soni
+Project Name: Online QRCode Generator
+"""
+import streamlit as st
+from PIL import Image
+import qrcode
+from matplotlib import colors as mcolors
+import streamlit.components.v1 as components
+
+html_code = """
+        <div style="background-color: #3cba54 ; padding:  10px; border-radius: 10px">
+          <h1 style="color:white; text-align: center">QR Code Generator</h1>
+        </div>
+        """
+components.html(html_code)
+
+
+img = Image.open("qrimg.jpg")
+st.image(img, width=700)
+
+html_code = """
+        <div style="background-color: #3cba54 ; padding:  10px; border-radius: 10px">
+          <h1 style="color:white; text-align: center">QR Code Generator</h1>
+        </div>
+        """
+components.html(html_code)
+
+
+# Add the data to QR
+data = st.text_input("Enter the data you want to embed")
+data_title = data.title()
+
+
+# Number Inputs
+box_size = st.number_input("Enter the box size")
+border = st.number_input("Enter the border spacing value")
+
+# Select Box Inputs
+
+colors = mcolors.CSS4_COLORS
+colors_list = list(colors.keys())
+
+fill = st.selectbox("Select the fill color", colors_list)
+back = st.selectbox("Select the background color", colors_list)
+
+
+# Create a QR Code
+
+if(st.button("Generate QR Code")):
+    qr = qrcode.QRCode(
+        box_size=box_size,
+        border=border
+    )
+    
+    qr.add_data(data_title)
+    qr.make()
+    img = qr.make_image(fill_color = fill, back_color = back)
+    img.save('myqr.png')
+    
+    
+    # Display the QR Code
+    img = Image.open("myqr.png")
+    st.image(img)
